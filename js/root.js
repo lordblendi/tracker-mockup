f2307900cca6224aafd0d6e224f1f6ad32e96569
Nóra Szepes <nora.szepes@tenforce.com>
2017-11-08 13:40:34 +0100

    added initial files

---
+++ js/root.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+  var $app = $('.app');
+
+  // Toggle NAVIGATION
+  $('.top-bar__left').click(function() {
+    $app.addClass('app__nav--visible');
+  });
+  $('.top-bar__itemAccount').click(function() {
+    $app.addClass('notifications--visible');
+  });
+  $('.app__overlay').click(function() {
+    if ($app.hasClass('app__nav--visible')) {
+      $app.removeClass('app__nav--visible');
+    }
+    if ($app.hasClass('notifications--visible')) {
+      $app.removeClass('notifications--visible');
+    }
+    if ($app.hasClass('overlay--visible')) {
+      $app.removeClass('overlay--visible');
+
+      $('.breadcrumb').removeClass('active');
+    }
+    if ($app.hasClass('assistance--visible')) {
+      $app.removeClass('assistance--visible');
+      $('.assistance').removeClass('visible');
+    }
+  });
+
+  // Toggle COMMENTS-ATTACHMENTS-HISTORY
+  $('.media--toggle').click(function() {
+    $app.toggleClass('document__comments--visible');
+  });
+
+  // Toggle INFO
+  $('.process--toggle').click(function() {
+    $app.toggleClass('document__info--visible');
+  });
+
+
+
+  $('.document__comments .add-comment .add-comment__input').focus(function() {
+    $app.addClass('add-comment--visible');
+  });
+
+  $('.document__comments .add-comment .add-comment__input').blur(function() {
+    $app.removeClass('add-comment--visible');
+  });
+
+
+  // breadcrum symbol
+  $('.breadcrumb .title .link').click(function() {
+    $app.toggleClass('overlay--visible');
+
+    $(this).parent().parent('.breadcrumb').toggleClass('active');
+    //$('.assistance').addClass('visible');
+  });
+
+
+
+
+
+
+  // Sliders
+  $('.sliders .slider .slider__header .toggle').click(function(e) {
+      e.preventDefault();
+
+      if($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $(this).parent().parent().find('.slider__body').slideUp();
+      }
+      else {
+        $(this).addClass('active');
+        $(this).parent().parent().find('.slider__body').slideDown();
+      }
+
+
+  });
+
+
+
+
+
+
+  /// TO CHECK
+  // assistance
+    $('.assistance--toggle').click(function() {
+      $app.addClass('assistance--visible');
+
+      $('.assistance').addClass('visible');
+    });
+
+}); // END
+
+
+/* Change doc types */
+/*
+(function(f){
+  var doctypes = ['story', 'bug', 'meeting'];
+  var i = 0;
+  var int = window.setInterval(function(){
+    updateBadge(i++);
+
+    if(i === 3) {
+      i = 0;
+    }
+  }, 5000);
+
+  var badgeNum;
+
+  function updateBadge(alertNum) {
+    $('.app__document').removeClass('app__document--story');
+    $('.app__document').removeClass('app__document--bug');
+    $('.app__document').removeClass('app__document--meeting');
+
+    $('.app__document').addClass('app__document--' + doctypes[alertNum]);
+    $('.app__document .document__visual-identifier span').html('' + doctypes[alertNum])
+  }
+})(document);
+*/
