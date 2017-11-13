60185b4ed69cd30d6548fddb9a2e67e37bcd2233
Nóra Szepes <nora.szepes@tenforce.com>
2017-11-13 17:13:50 +0100

    added sticky header for tables

--- js/plugin/scroll.js
+++ js/plugin/scroll.js
@@ -1,12 +1,15 @@
 $(function() {
   $(document).ready(function() {
+
+    $('table').stickyTableHeaders({fixedOffset: $(".sticky__header__wrapper").height() + $(".sticky__header__wrapper").height()});
+
     $(".sticky__header__wrapper").sticky({
       zIndex: 10
     });
 
     $(".sticky__document__header").sticky({
       zIndex: 10,
-      topSpacing: $(".sticky__header__wrapper").height() + 10
+      topSpacing: $(".sticky__header__wrapper").height()
     });
   });
 });
