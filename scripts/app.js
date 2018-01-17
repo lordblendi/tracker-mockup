f393bba94f6441659f65f9d577457cf0ce6562f7
Nick De Jaeger <nick.de.jaeger@tenforce.com>
2018-01-17 16:11:18 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 document.ready(function() {
 
-  
+  var setHeightDocumentContent = function() {
+    var check = 0;
+
+    $('*[class^="panel__"]').each(function() {
+        var _this = $(this).height();
+
+        if (_this >= check) {
+            check = _this;
+        }
+    });
+
+    $('.document__content').css('height', check + 'px');
+  }();
 
 }); // END
