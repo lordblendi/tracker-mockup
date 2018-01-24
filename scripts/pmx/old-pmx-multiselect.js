f9382e615264537366e8be88921ee57bbf3a1d1a
Nóra Szepes <nora.szepes@tenforce.com>
2018-01-24 16:37:16 +0100

    added old multiselect: tabs include/exclude. tabs design. bit of exclude design too.

--- scripts/pmx/old-pmx-multiselect.js
+++ scripts/pmx/old-pmx-multiselect.js
@@ -1,3 +1,17 @@
 $('.old-pmx-multiselect-trigger').click(function(){
   $('.old-multiselect-popup').toggleClass('old-multiselect-popup--hidden');
 });
+
+
+$('.old-multiselect-popup .old-multiselect-tabs ul li').click(function(){
+  var li = $(this);
+  if(!li.hasClass('selected')) {
+    var ul = li.closest('ul');
+    var tabSelector = $(ul).find('.tab-selector');
+    tabSelector.toggleClass('selected');
+
+    var newpickercontent = $(ul).closest('.newpickercontent');
+    var oldMultiselectTab = $(newpickercontent).find('.old-multiselect-tab');
+    oldMultiselectTab.toggleClass('old-multiselect-tab--hidden');
+  }
+});
