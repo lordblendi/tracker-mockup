63a4d74c215556deefc6afea080e914263e789c2
Nóra Szepes <nora.szepes@tenforce.com>
2018-01-25 13:59:54 +0100

    trigger to open new multiselector

--- scripts/pmx/old-pmx-multiselect.js
+++ scripts/pmx/old-pmx-multiselect.js
@@ -1,11 +1,11 @@
-$('.old-pmx-multiselect-trigger').click(function(){
+$('.old-pmx-multiselect-trigger').click(function() {
   $('.old-multiselect-popup').toggleClass('old-multiselect-popup--hidden');
 });
 
 
-$('.old-multiselect-popup .old-multiselect-tabs ul li').click(function(){
+$('.old-multiselect-popup .old-multiselect-tabs ul li').click(function() {
   var li = $(this);
-  if(!li.hasClass('selected')) {
+  if (!li.hasClass('selected')) {
     var ul = li.closest('ul');
     var tabSelector = $(ul).find('.tab-selector');
     tabSelector.toggleClass('selected');
@@ -15,3 +15,20 @@ $('.old-multiselect-popup .old-multiselect-tabs ul li').click(function(){
     oldMultiselectTab.toggleClass('old-multiselect-tab--hidden');
   }
 });
+
+$('.new-multiselect-trigger').click(function() {
+  var trigger = $(this);
+  const height = trigger.outerHeight();
+  const width = trigger.outerWidth();
+  const top = trigger.offset().top;
+  const left = trigger.offset().left;
+  const multiselectWidth = $('#multiSelector-2').outerWidth();
+
+  const newWidth = left + width - multiselectWidth;
+  const newHeight = top + height;
+  $('#multiSelector-2').css('left', newWidth);
+  $('#multiSelector-2').css('top', newHeight);
+  $('#multiSelector-2').toggleClass('multiSelector--visible');
+});
+
+// multiSelector--visible
