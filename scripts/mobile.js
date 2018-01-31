7944b274b51aad040fb9836ff5c041d83105cf5c
Nóra Szepes <nora.szepes@tenforce.com>
2018-01-31 13:40:36 +0100

    added left panel for mobile view. swapped backgrounds

---
+++ scripts/mobile.js
@@ -0,0 +1,28 @@
+$('.pmx-selector__value--comments, .pmx-selector__value--timeline').click(function(){
+  const button = $(this);
+  const timeline = button.hasClass('pmx-selector__value--timeline');
+  const comments = button.hasClass('pmx-selector__value--comments');
+  const selected = button.hasClass('pmx-selector__value--selected');
+
+  const mobileContainer = $('main.mobile__container');
+
+  if(comments) {
+    if(selected) {
+      mobileContainer.addClass("mobile_panel--left");
+    }
+    else {
+      mobileContainer.removeClass("mobile_panel--left");
+    }
+    mobileContainer.removeClass("mobile_panel--right");
+  }
+  else if (timeline) {
+    if(selected) {
+      mobileContainer.addClass("mobile_panel--right");
+    }
+    else {
+      mobileContainer.removeClass("mobile_panel--right");
+    }
+    mobileContainer.removeClass("mobile_panel--left");
+  }
+
+});
