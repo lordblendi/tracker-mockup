82b1634d7f724d5cadc2ef456c07c19acc359b45
Nick De Jaeger <nick.de.jaeger@tenforce.com>
2018-02-01 16:15:17 +0100

    show blue search icon on semsearc active

--- scripts/components/semsearch.js
+++ scripts/components/semsearch.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
   $('[data-action="open-semsearch"]').on('mouseover', function() {
+    $(this).addClass('shown');
     $('[data-popup="semsearch"]').addClass('multiSelector--visible');
   });
 
   $('[data-popup="semsearch"]').on('mouseout', function() {
+    $('[data-action="open-semsearch"]').removeClass('shown');
     $(this).removeClass('multiSelector--visible');
   });
 
