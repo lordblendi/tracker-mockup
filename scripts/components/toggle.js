f90b974f29dbf35b878eee2fb600a2b01f1705d5
Nick De Jaeger <nick.de.jaeger@tenforce.com>
2018-02-09 12:35:41 +0100

    uber update

--- scripts/components/toggle.js
+++ scripts/components/toggle.js
@@ -11,12 +11,12 @@ $('.pmx-selector').on('click', function() {
 
 
 // toggle button actions
-$('.pmx-selector__value').on('click', function() {
+$('.pmx-selector__item').on('click', function() {
   const selectorValue = $(this);
   const selector = selectorValue.closest('.pmx-selector');
 
   if(!selector.hasClass('MULTISELECT__POPUP')) {
-    const isSelected = selectorValue.hasClass('pmx-selector__value--selected');
+    const isSelected = selectorValue.hasClass('pmx-selector__item--selected');
 
     const required = selector.hasClass('pmx-selector--required');
     const expanded = selector.hasClass('pmx-selector--expanded');
@@ -25,26 +25,26 @@ $('.pmx-selector__value').on('click', function() {
     const toggleSelect = selector.hasClass('pmx-selector--toggleSelect');
     const inclExcl = selector.hasClass('js_pmx-selector--incl-excl');
 
-    const selectedValues = selector.find('.pmx-selector__value--selected');
+    const selectedValues = selector.find('.pmx-selector__item--selected');
 
     if(singleSelect === true) {
 
     }
     else if (toggleSelect === true) {
       if(required === true) {
-        selectedValues.removeClass('pmx-selector__value--selected');
-        selectorValue.addClass('pmx-selector__value--selected');
+        selectedValues.removeClass('pmx-selector__item--selected');
+        selectorValue.addClass('pmx-selector__item--selected');
       }
       else {
         if(!isSelected) {
-          selectorValue.toggleClass('pmx-selector__value--selected');
+          selectorValue.toggleClass('pmx-selector__item--selected');
         }
-        selectedValues.removeClass('pmx-selector__value--selected');
+        selectedValues.removeClass('pmx-selector__item--selected');
       }
 
       if(selector.hasClass('js_pmx-selector--changeIcons')) {
         selectedValues.find('.pmx-selector__icon').html("-");
-        if(selectorValue.hasClass('pmx-selector__value--selected')) {
+        if(selectorValue.hasClass('pmx-selector__item--selected')) {
           selectorValue.find('.pmx-selector__icon').html("+");
         }
         else {
@@ -58,15 +58,15 @@ $('.pmx-selector__value').on('click', function() {
       if(required === true) {
         if(selectedValues.length === 1) {
           if (isSelected === false) {
-            selectorValue.toggleClass('pmx-selector__value--selected');
+            selectorValue.toggleClass('pmx-selector__item--selected');
           }
         }
         else {
-          selectorValue.toggleClass('pmx-selector__value--selected');
+          selectorValue.toggleClass('pmx-selector__item--selected');
         }
       }
       else {
-        selectorValue.toggleClass('pmx-selector__value--selected');
+        selectorValue.toggleClass('pmx-selector__item--selected');
       }
     }
 
@@ -75,7 +75,7 @@ $('.pmx-selector__value').on('click', function() {
     if(inclExcl === true) {
       // old multiselect
       if (selector.hasClass('js_pmx-selector--old')) {
-        if(selectorValue.hasClass('js_pmx-selector__value--exclude') && selectorValue.hasClass('pmx-selector__value--selected')) {
+        if(selectorValue.hasClass('js_pmx-selector__item--exclude') && selectorValue.hasClass('pmx-selector__item--selected')) {
           $('.old-multiselect-popup .old-multiselect-tab').addClass('js_old-multiselect-tab-negative');
         }
         else {
@@ -84,7 +84,7 @@ $('.pmx-selector__value').on('click', function() {
       }
       //  new multiselect
       else {
-        if(selectorValue.hasClass('js_pmx-selector__value--exclude') && selectorValue.hasClass('pmx-selector__value--selected')) {
+        if(selectorValue.hasClass('js_pmx-selector__item--exclude') && selectorValue.hasClass('pmx-selector__item--selected')) {
           selectorValue.closest('.multiSelector__box').addClass('js_exclude');
         }
         else {
