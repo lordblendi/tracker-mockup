cd6ebf03a61e71be17bde83d7347d16006f8b1c8
Nóra Szepes <nora.szepes@tenforce.com>
2018-02-13 15:22:36 +0100

    more intelligent filter function - not bound by ID to input field

--- scripts/plugin/filter.js
+++ scripts/plugin/filter.js
@@ -1,18 +1,16 @@
-function tagFilter() {
-
-
-  var input = document.getElementById('tagFilter');
-  var filter = input.value.toUpperCase();
+$('.js_multiSelector__filterInput').keyup(function() {
+  var input = $(this);
+  var filter = input[0].value.toUpperCase();
   var multiSelector = $(input).closest(".multiSelector");
   var listItems = $(multiSelector).find('.js_multiSelector__box--selectionChildren .js_filterableCell, .js_multiSelector__box--optionsChildren .js_filterableCell');
 
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < listItems.length; i++) {
-        var text = $(listItems[i]).find(".js_itemBoxTable__bodyCellInner--text")[0];
-        if (text.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            listItems[i].style.display = "";
-        } else {
-            listItems[i].style.display = "none";
-        }
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < listItems.length; i++) {
+    var text = $(listItems[i]).find(".js_itemBoxTable__bodyCellInner--text")[0];
+    if (text.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      listItems[i].style.display = "";
+    } else {
+      listItems[i].style.display = "none";
     }
-}
+  }
+});
