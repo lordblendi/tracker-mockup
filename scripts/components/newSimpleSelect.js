8801e6d75ae447c8ea6578d58f55d3b5a678ca1c
Nóra Szepes <nora.szepes@tenforce.com>
2018-02-14 15:49:17 +0100

    sepated multiselect.js into smaller js files

---
+++ scripts/components/newSimpleSelect.js
@@ -0,0 +1,21 @@
+//////
+// MULTI SELECT ONCLICK
+//////
+//
+// SIMPLE
+// $('.MULTISELECT__POPUP[data-popid="#multiSelector-1"]')
+$('#multiSelector-1 .itemBoxTable__bodyRow').on('click', function(){
+  const row = $(this);
+  const newLabel = row.find('.js_itemBoxTable__bodyCell--text .itemBoxTable__bodyCellInner').html();
+
+  const selectorField = $('.MULTISELECT__POPUP[data-popid="#multiSelector-1"]');
+  const selectorLabelField = $(selectorField).find('.js_pmx-selector__blockPart--label');
+  $(selectorLabelField).html(newLabel);
+
+  $(".overlay").css("display", 'none');
+  $('#multiSelector-1, #multiSelector-2, #multiSelector-3').css({
+    'z-index': '0',
+    'pointer-events': 'none',
+    'opacity': '0',
+  });
+})
