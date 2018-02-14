78b1a6b349b5a8d682b7a0824ebba0aac05db8c4
Nóra Szepes <nora.szepes@tenforce.com>
2018-02-14 15:41:57 +0100

    newly selected item can also change color now

--- scripts/components/newMultiselect.js
+++ scripts/components/newMultiselect.js
@@ -430,6 +430,7 @@ function handleComplexItemAddRemove(action, exclude){
 }
 
 function resetColorToggle(item) {
+  // expand/collapse for new selected item
   $(item).find('.js_itemBoxTable__bodyCellInner--colortoggle').on('click', function() {
     // if there is a color box, expand/collapse it
     const itemBoxTable__bodyRow = $(this).closest('.itemBoxTable__bodyRow');
@@ -442,6 +443,25 @@ function resetColorToggle(item) {
   const possibleColorChildren = $(item).next('.js_itemBox--colors');
   possibleColorChildren.css('display', 'none').addClass('js_itemBoxTable__bodyRow--closed');
   $(item).addClass('js_itemBoxTable__bodyRow--closed');
+
+  $(possibleColorChildren).find('.itemBoxTable__bodyRow').on('click', function() {
+    const color = $(this).find('.js_itemBoxTable__bodyCellInner--color i').attr('data-color');
+    const itemBox = $(this).closest('.js_itemBox--colors');
+    const bodyRow = $(itemBox).prev();
+
+    bodyRow.find('.js_colorselector-trigger i').css('color', color);
+
+    $('.itemBoxTable__bodyCell--active').each(function() {
+      $(this).removeClass('itemBoxTable__bodyCell--active');
+    });
+
+
+    $.Velocity.animate(itemBox, 'slideUp').then(function() {
+      $(colorBodyRow).addClass('js_itemBoxTable__bodyRow--closed');
+      $(itemBox).addClass('js_itemBoxTable__bodyRow--closed');
+    });
+  });
+
 }
 
 function checkGroupActions() {
