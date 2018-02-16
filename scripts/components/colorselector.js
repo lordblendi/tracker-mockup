c9aff8a2f8e7191a277a02d7c33a9a384a30900c
Nóra Szepes <nora.szepes@tenforce.com>
2018-02-16 10:56:01 +0100

    Colorselector: same color for all tags with same text; refactoring

--- scripts/components/colorselector.js
+++ scripts/components/colorselector.js
@@ -1,33 +1,62 @@
 // COLOR OPTIONS SELECTION
 $('.js_itemBox--colors .itemBoxTable__bodyRow').on('click', function() {
-  const color = $(this).find('.js_itemBoxTable__bodyCellInner--color i').attr('data-color');
-  const itemBox = $(this).closest('.js_itemBox--colors');
+  const colorItemBodyRow = $(this);
+  selectNewColor(colorItemBodyRow);
+});
+
+function selectNewColor(colorItemBodyRow) {
+  // get new color
+  const color = colorItemBodyRow.find('.js_itemBoxTable__bodyCellInner--color i').attr('data-color');
+  // find the text of the item
+  const itemBox = colorItemBodyRow.closest('.js_itemBox--colors');
   const bodyRow = $(itemBox).prev();
+  const tagText = bodyRow.find('.js_itemBoxTable__bodyCellInner--text').html().trim();
 
-  bodyRow.find('.js_colorselector-trigger i').css('color', color).attr('data-color', color);
+  // find all tags with the same text
+  const multiSelector = bodyRow.closest('.multiSelector');
+  const possibleTags = multiSelector.find('.js_itemBoxTable__bodyCellInner--text');
+  var filteredTags = [];
 
-  $('.itemBoxTable__bodyCell--active').each(function() {
-    $(this).removeClass('itemBoxTable__bodyCell--active');
+  $.each(possibleTags, function(index, tag){
+    if($(tag).html().trim() === tagText) {
+      filteredTags.push(tag);
+    }
   });
 
+  // change color for all other tags, that have the same textOfActionItem
+  $.each(filteredTags, function(index, tag){
+    const tagBodyRow = $(tag).closest('.itemBoxTable__bodyRow');
+    tagBodyRow.find('.js_colorselector-trigger i').css('color', color).attr('data-color', color);
+  });
 
+  // remove active cells
+  $('.itemBoxTable__bodyCell--active').each(function() {
+    colorItemBodyRow.removeClass('itemBoxTable__bodyCell--active');
+  });
+
+  // close color options
   $.Velocity.animate(itemBox, 'slideUp').then(function() {
     $(itemBox).addClass('js_itemBoxTable__bodyRow--closed');
   });
-});
+}
 
 
 // COLOR OPTIONS EXPAND-COLLAPSE
 $('.js_itemBoxTable__bodyCellInner--colortoggle').on('click', function() {
+  const colorToggle = $(this);
+  toggleColorSelector(colorToggle);
+});
+
+function toggleColorSelector(colorToggle) {
   // if there is a color box, expand/collapse it
-  const itemBoxTable__bodyRow = $(this).closest('.itemBoxTable__bodyRow');
+  const itemBoxTable__bodyRow = colorToggle.closest('.itemBoxTable__bodyRow');
   const possibleChildren = itemBoxTable__bodyRow.next('.js_itemBox--colors');
 
-  $(this).closest('.itemBoxTable__bodyCell').toggleClass('itemBoxTable__bodyCell--active');
+  colorToggle.closest('.itemBoxTable__bodyCell').toggleClass('itemBoxTable__bodyCell--active');
   if(possibleChildren.length > 0) {
     expandCloseRow(itemBoxTable__bodyRow, undefined, possibleChildren);
   }
-});
+}
 
 
 
@@ -44,35 +73,18 @@ $.Velocity.animate(colorOptions, 'slideUp').then(function() {
 function resetColorToggle(item) {
   // expand/collapse for new selected item
   $(item).find('.js_itemBoxTable__bodyCellInner--colortoggle').on('click', function() {
-    // if there is a color box, expand/collapse it
-    const itemBoxTable__bodyRow = $(this).closest('.itemBoxTable__bodyRow');
-    const possibleChildren = itemBoxTable__bodyRow.next('.js_itemBox--colors');
-
-    $(this).closest('.itemBoxTable__bodyCell').toggleClass('itemBoxTable__bodyCell--active');
-    if(possibleChildren.length > 0) {
-      expandCloseRow(itemBoxTable__bodyRow, undefined, possibleChildren);
-    }
+    const colorToggle = $(this);
+    toggleColorSelector(colorToggle);
   });
 
+  // set colorselector hidden
   const possibleColorChildren = $(item).next('.js_itemBox--colors');
   possibleColorChildren.css('display', 'none').addClass('js_itemBoxTable__bodyRow--closed');
   $(item).addClass('js_itemBoxTable__bodyRow--closed');
 
   $(possibleColorChildren).find('.itemBoxTable__bodyRow').on('click', function() {
-    const color = $(this).find('.js_itemBoxTable__bodyCellInner--color i').attr('data-color');
-    const itemBox = $(this).closest('.js_itemBox--colors');
-    const bodyRow = $(itemBox).prev();
-
-    bodyRow.find('.js_colorselector-trigger i').css('color', color);
-
-    $('.itemBoxTable__bodyCell--active').each(function() {
-      $(this).removeClass('itemBoxTable__bodyCell--active');
-    });
-
-
-    $.Velocity.animate(itemBox, 'slideUp').then(function() {
-      $(itemBox).addClass('js_itemBoxTable__bodyRow--closed');
-    });
+    const colorItemBodyRow = $(this);
+    selectNewColor(colorItemBodyRow);
   });
 
 }
