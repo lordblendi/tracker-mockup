a430cee5a99fc6cd6519d94837f9cdf49f0de5f8
Nóra Szepes <nora.szepes@tenforce.com>
2018-02-16 15:55:13 +0100

    Comment, refactor: selector.js

--- scripts/components/selector.js
+++ scripts/components/selector.js
@@ -3,45 +3,41 @@ $('.pmx-selector').on('click', function() {
   $(this).toggleClass('pmx-selector--clicked');
 });
 
-
-
-
-
-
-
-
 // toggle button actions
 $('.pmx-selector__item').on('click', function() {
   const selectorValue = $(this);
   const selector = selectorValue.closest('.pmx-selector');
 
+  // only if this is not a multiselect popup toggle
   if(!selector.hasClass('MULTISELECT__POPUP')) {
     const isSelected = selectorValue.hasClass('pmx-selector__item--selected');
 
     const required = selector.hasClass('pmx-selector--required');
     const expanded = selector.hasClass('pmx-selector--expanded');
     const multiSelect = selector.hasClass('pmx-selector--multiSelect');
-    const singleSelect = selector.hasClass('pmx-selector--singleSelect');
     const toggleSelect = selector.hasClass('pmx-selector--toggleSelect');
     const inclExcl = selector.hasClass('js_pmx-selector--incl-excl');
 
     const selectedValues = selector.find('.pmx-selector__item--selected');
 
-    if(singleSelect === true) {
-
-    }
-    else if (toggleSelect === true) {
+    // toggleSelect = singleSelect
+    if (toggleSelect === true) {
+      // if required, remove all the others from being selected
+      selectedValues.removeClass('pmx-selector__item--selected');
+      // if required, select the one you clicked on
       if(required === true) {
-        selectedValues.removeClass('pmx-selector__item--selected');
         selectorValue.addClass('pmx-selector__item--selected');
       }
+      // otherwise if it's not selected, toggle it;
       else {
         if(!isSelected) {
           selectorValue.toggleClass('pmx-selector__item--selected');
         }
-        selectedValues.removeClass('pmx-selector__item--selected');
       }
 
+      // if it has to change icons -> one specific toggle example
+      // swap between - and +
+      // TEMPORARY
       if(selector.hasClass('js_pmx-selector--changeIcons')) {
         selectedValues.find('.pmx-selector__blockPart--prefix').html("-");
         if(selectorValue.hasClass('pmx-selector__item--selected')) {
@@ -65,6 +61,7 @@ $('.pmx-selector__item').on('click', function() {
           selectorValue.toggleClass('pmx-selector__item--selected');
         }
       }
+      // otherwise just toggle
       else {
         selectorValue.toggleClass('pmx-selector__item--selected');
       }
