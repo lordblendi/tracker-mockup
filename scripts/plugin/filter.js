a4ed59d01e9ceed081a0ef1b2ad9812bb873f3ab
Nóra Szepes <nora.szepes@tenforce.com>
2018-02-19 16:05:42 +0100

    Filter: refactor into new function

--- scripts/plugin/filter.js
+++ scripts/plugin/filter.js
@@ -1,31 +1,9 @@
+//  filter for multiselects
 $('.js_multiSelector__filterInput').keyup(function() {
   var input = $(this);
-  var filter = input[0].value.toUpperCase();
   var multiSelector = $(input).closest(".multiSelector");
-  var listItems = $(multiSelector).find('.js_multiSelector__box--selectionChildren .js_filterableCell, .js_multiSelector__box--optionsChildren .js_filterableCell');
+  filterResults(input, '.js_multiSelector__box--selectionChildren .js_filterableCell, .js_multiSelector__box--optionsChildren .js_filterableCell');
 
-  if(filter.length > 0) {
-    $(multiSelector).find('.js_itemBox--suggestions').css('display', 'block');
-  }
-  else {
-    $(multiSelector).find('.js_itemBox--suggestions').css('display', 'none');
-  }
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < listItems.length; i++) {
-    var text = $(listItems[i]).find(".js_itemBoxTable__bodyCellInner--text")[0];
-    if (text.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      listItems[i].style.display = "";
-      listItems[i].outerHTML = listItems[i].outerHTML.trim()
-                                     .replace('<span ','<ul ')
-                                     .replace('</span>','</ul');
-    } else {
-      listItems[i].style.display = "none";
-      listItems[i].outerHTML = listItems[i].outerHTML.trim()
-                                     .replace('<ul ','<span ')
-                                     .replace('</ul>','</span');
-    }
-  }
 
   // check all groups, see if they need to be hidden or not
   var groups = $(multiSelector).find('.js_multiSelector__box--selectionChildren  .itemBoxTable__bodyRow:not(.js_filterableCell), .js_multiSelector__box--optionsChildren  .itemBoxTable__bodyRow:not(.js_filterableCell)');
@@ -56,6 +34,28 @@ $('.js_multiSelector__filterInput').keyup(function() {
 
     reInitActions();
 });
+
+function filterResults(input, listItemSelector) {
+  var filter = input[0].value.toUpperCase();
+  var multiSelector = $(input).closest(".multiSelector");
+  var listItems = $(multiSelector).find(listItemSelector);
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < listItems.length; i++) {
+    var text = $(listItems[i]).find(".js_itemBoxTable__bodyCellInner--text")[0];
+    if (text.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      listItems[i].style.display = "";
+      listItems[i].outerHTML = listItems[i].outerHTML.trim()
+                                     .replace('<span ','<ul ')
+                                     .replace('</span>','</ul');
+    } else {
+      listItems[i].style.display = "none";
+      listItems[i].outerHTML = listItems[i].outerHTML.trim()
+                                     .replace('<ul ','<span ')
+                                     .replace('</ul>','</span');
+    }
+  }
+}
 
 // we have to reinit all actions
 function reInitActions() {
