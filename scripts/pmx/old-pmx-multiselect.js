32316dd97e2ad9f2fba341e737f7843b145e799d
Nóra Szepes <nora.szepes@tenforce.com>
2018-02-19 13:09:57 +0100

    Overlay: added functionality for /pmx

--- scripts/pmx/old-pmx-multiselect.js
+++ scripts/pmx/old-pmx-multiselect.js
@@ -1,20 +1,30 @@
 $('.js_old-pmx-multiselect-trigger').click(function() {
-  $('.old-multiselect-popup').toggleClass('old-multiselect-popup--hidden');
-});
-
-
-$('.old-multiselect-popup .old-multiselect-tabs ul li').click(function() {
-  var li = $(this);
-  if (!li.hasClass('selected')) {
-    var ul = li.closest('ul');
-    var tabSelector = $(ul).find('.tab-selector');
-    tabSelector.toggleClass('selected');
-
-    var newpickercontent = $(ul).closest('.newpickercontent');
-    var oldMultiselectTab = $(newpickercontent).find('.old-multiselect-tab');
-    oldMultiselectTab.toggleClass('old-multiselect-tab--hidden');
+  var oldpopup = $('.old-multiselect-popup');
+  // if hidden, show it and add overlay
+  if(oldpopup.hasClass('old-multiselect-popup--hidden')) {
+    oldpopup.removeClass('old-multiselect-popup--hidden');
+    $(".overlay").css("display", 'block');
+  }
+  // if not hidden, hide it, close overlay
+  else {
+    oldpopup.addClass('old-multiselect-popup--hidden');
+    closeOverlay();
   }
 });
+
+//
+// $('.old-multiselect-popup .old-multiselect-tabs ul li').click(function() {
+//   var li = $(this);
+//   if (!li.hasClass('selected')) {
+//     var ul = li.closest('ul');
+//     var tabSelector = $(ul).find('.tab-selector');
+//     tabSelector.toggleClass('selected');
+//
+//     var newpickercontent = $(ul).closest('.newpickercontent');
+//     var oldMultiselectTab = $(newpickercontent).find('.old-multiselect-tab');
+//     oldMultiselectTab.toggleClass('old-multiselect-tab--hidden');
+//   }
+// });
 
 $('.js_new-multiselect-trigger').click(function() {
   var trigger = $(this);
@@ -28,9 +38,23 @@ $('.js_new-multiselect-trigger').click(function() {
 
   const newWidth = left + width - multiselectWidth;
   const newHeight = top + height;
-  $(multiselectId).css('left', newWidth);
-  $(multiselectId).css('top', newHeight);
+  $(multiselectId).css({
+    'z-index': '99999999',
+    'pointer-events': 'auto',
+    'opacity': '1',
+    'top': newHeight,
+    'left': newWidth,
+  });
   $(multiselectId).toggleClass('multiSelector--visible');
+  // show overlay
+  $(".overlay").css("display", 'block');
 });
 
 // multiSelector--visible
+
+// onclick action on overlay to close popups
+$('.overlay').on('click', function() {
+  $('.multiSelector--popup.multiSelector--visible').removeClass('multiSelector--visible');
+
+  $('.old-multiselect-popup').addClass('old-multiselect-popup--hidden');
+});
