5053eddefdfb4a733d01d83dba442839a53b2610
Nóra Szepes <nora.szepes@tenforce.com>
2018-02-20 14:46:22 +0100

    Filter: Only swap ul <-> span if it's at the begining/end of string
    otherwise intermediate things will get swapped too

--- scripts/plugin/filter.js
+++ scripts/plugin/filter.js
@@ -30,16 +30,19 @@ function filterResults(input, listItemSelector) {
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < listItems.length; i++) {
     var text = $(listItems[i]).find(".js_itemBoxTable__bodyCellInner--text")[0];
-    if (text.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      listItems[i].style.display = "";
-      listItems[i].outerHTML = listItems[i].outerHTML.trim()
-                                     .replace('<span ','<ul ')
-                                     .replace('</span>','</ul');
-    } else {
-      listItems[i].style.display = "none";
-      listItems[i].outerHTML = listItems[i].outerHTML.trim()
-                                     .replace('<ul ','<span ')
-                                     .replace('</ul>','</span');
+
+    if(text !== undefined) {
+      if (text.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        listItems[i].style.display = "";
+        listItems[i].outerHTML = listItems[i].outerHTML.trim()
+        .replace('^<span ','<ul ')
+        .replace('</span>$','</ul');
+      } else {
+        listItems[i].style.display = "none";
+        listItems[i].outerHTML = listItems[i].outerHTML.trim()
+        .replace('^<ul ','<span ')
+        .replace('</ul>$','</span');
+      }
     }
   }
 }
