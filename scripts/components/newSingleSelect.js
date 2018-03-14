da04c4c9207831b1829f1cca189eae8f396e8a8f
Nóra Szepes <lordblendi@gmail.com>
2018-03-14 09:06:13 +0100

    Components: Single status select: don't close on clicking in the input field

--- scripts/components/newSingleSelect.js
+++ scripts/components/newSingleSelect.js
@@ -9,12 +9,15 @@ $('#multiSelector-1 .itemBoxTable__bodyRow.JS_filterableCell').on('click', funct
 // replace value in the selectorField
 // and close overlay
 function selectNewSingleItem(row) {
+  // if there is a filter input field, exit the function as we should not close the popup on entering the input field
+  if(row.find('.JS_multiSelector__filterInput').length > 0) {
+    return;
+  }
   const newLabel = row.find('.JS_itemBoxTable__bodyCell--text .itemBoxTable__bodyCellInner').html();
 
   const selectorField = $('.MULTISELECT__POPUP[data-popid="#multiSelector-1"]');
   const selectorLabelField = $(selectorField).find('.JS_pmx-selector__blockPart--label');
   $(selectorLabelField).html(newLabel);
-
   closeOverlay();
 
   // set filter to empty, as we selected stuff
