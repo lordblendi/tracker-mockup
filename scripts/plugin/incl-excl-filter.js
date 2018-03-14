79b8d3002c58c40bf396ad1dc40d7b8c896172c3
Nóra Szepes <lordblendi@gmail.com>
2018-03-14 12:58:58 +0100

    Filter: Added function to choose incl/excl in optionChildren

--- scripts/plugin/incl-excl-filter.js
+++ scripts/plugin/incl-excl-filter.js
@@ -1,6 +1,36 @@
 ---
 ---
 
+// SELECT INCL/EXCL
+$('.JS_itemBox--filterInclExcl .itemBoxTable__bodyRow').on('click', function() {
+  const incleExclBodyRow = $(this);
+  selectNewFilterOption(incleExclBodyRow);
+});
+
+// function to set a new filter option
+// gets old filter option, replaces in the bodyRow
+// then close the pallette
+function selectNewFilterOption(incleExclBodyRow) {
+  // get new option
+  const newOption = incleExclBodyRow.find('.JS_itemBoxTable__bodyCellInner--inclExcl').attr('data-filter');
+
+  // find the bodyRow related to this toggle
+  const itemBox = incleExclBodyRow.closest('.JS_itemBox--filterInclExcl');
+  const bodyRow = $(itemBox).prevAll('ul.itemBoxTable__bodyRow.JS_filterableCell:first');
+
+  // find toggle and replace text and data-filter
+  const toggle = $(bodyRow).find('.JS_itemBoxTable__bodyCellInner--inclexcltoggle i');
+  toggle.html(newOption).attr('data-filter', newOption);
+
+  // remove active cell from bodyRow as we are closing the selector
+  $(bodyRow).find('.itemBoxTable__bodyCell--active').removeClass('itemBoxTable__bodyCell--active');
+
+  // close options
+  $.Velocity.animate(itemBox, 'slideUp').then(function() {
+    $(itemBox).addClass('JS_itemBoxTable__bodyRow--closed');
+  });
+}
+
 // INCL-EXCL OPTIONS EXPAND-COLLAPSE
 $('.JS_itemBoxTable__bodyCellInner--inclexcltoggle').on('click', function() {
   const toggle = $(this);
@@ -8,6 +38,9 @@ $('.JS_itemBoxTable__bodyCellInner--inclexcltoggle').on('click', function() {
   toggleInclExclSelector(toggle);
 });
 
+// open color incl/excl pallette
+// set a tick for the selected option
+// toggle active class depending on expanding/collapsing
 function toggleInclExclSelector(toggle) {
   // if there is a color box, expand/collapse it
   const itemBoxTable__bodyRow = toggle.closest('.itemBoxTable__bodyRow');
