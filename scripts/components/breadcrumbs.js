c76cc1ed2ab6eaac50134e3ce1ea683e84a0a7cc
Nick De Jaeger <nick.de.jaeger@tenforce.com>
2018-03-29 13:28:59 +0200

    remove breadcrumb click effect

--- scripts/components/breadcrumbs.js
+++ scripts/components/breadcrumbs.js
@@ -2,15 +2,6 @@ $(document).ready(function() {
   var $app = $('.app');
 
   $('.breadcrumbs__item').on('click', function() {
-    $app.toggleClass('overlay--visible');
 
-    $(this).parent().toggleClass('breadcrumbs--active');
-    $(this).toggleClass('breadcrumbs__item--active');
-  });
-
-  $app.on('click', function() {
-    if( $(this).hasClass('overlay--visible') ) {
-      $('.breadcrumbs').removeClass('breadcrumbs--active');
-    }
   });
 });
