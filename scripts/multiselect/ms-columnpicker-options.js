7b30ecfa7042ab5653914c7d4feb323fed4d3e6d
Nóra Szepes <nora.szepes@tenforce.com>
2018-05-03 18:01:13 +0200

    added functionality for checking enabled column picker options

--- scripts/multiselect/ms-columnpicker-options.js
+++ scripts/multiselect/ms-columnpicker-options.js
@@ -1,3 +1,6 @@
+---
+---
+
 // CALENDAR OPTIONS EXPAND-COLLAPSE
 $('.JS_toggle--columnpickerOptions').on('click', function() {
   const toggle = $(this);
@@ -14,22 +17,37 @@ function toggleColumnpickerOptions(toggle) {
   const possibleChildren = itemBox__row.nextAll('.JS_itemBox--columnpickerOptions:first');
 
   // only toggle, if they actually exists;
-  if(possibleChildren.length > 0) {
-  // TODO when we need to show what is selected
+  if (possibleChildren.length > 0) {
+    // TODO when we need to show what is selected
 
-  const bodyCell = toggle.closest('.itemBox__cell');
-  if (bodyCell.hasClass('itemBox__cell--active')) {
-    bodyCell.removeClass('itemBox__cell--active');
-  }
-  else {
-    bodyCell.addClass('itemBox__cell--active');
-  }
+    const bodyCell = toggle.closest('.itemBox__cell');
+    if (bodyCell.hasClass('itemBox__cell--active')) {
+      bodyCell.removeClass('itemBox__cell--active');
+    } else {
+      bodyCell.addClass('itemBox__cell--active');
+    }
 
     expandCloseRow(itemBox__row, undefined, possibleChildren);
   }
 }
 
 
-$('.JS_itemBox--columnpickerOptions JS_selector').on('click', function(){
-
+$('.JS_itemBox--columnpickerOptions .JS_selectorItem').on('click', function() {
+  handleColumnPickerOptionChange($(this));
 });
+
+function handleColumnPickerOptionChange(selector) {
+  const itemBox = $(selector).closest('.JS_itemBox--columnpickerOptions');
+
+  const enabledOptions = $(itemBox).find('.JS_columnPickerOption--enabled.JS_selectorItem--active');
+
+  var columnPickerToggleContent = `{% include javascript/ms-columnpickerOption--disabled.html%}`;
+
+  if (enabledOptions.length > 0) {
+    columnPickerToggleContent = `{% include javascript/ms-columnpickerOption--enabled.html%}`;
+  }
+
+  const bodyRow = $(itemBox).prevAll('ul.itemBox__row.JS_filterableCell:first');
+  const toggle = $(bodyRow).find('.JS_toggle--columnpickerOptions');
+  toggle.html(columnPickerToggleContent);
+}
