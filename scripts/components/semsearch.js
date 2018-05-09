0d49157edcc108b99873463c0ec438e31c53ab51
Nóra Szepes <nora.szepes@tenforce.com>
2018-05-09 15:08:07 +0200

    Semsearch: position fix

--- scripts/components/semsearch.js
+++ scripts/components/semsearch.js
@@ -1,9 +1,25 @@
-$(document).ready(function() {
+$('.JS_toggle-semsearch').hover(function() {
+  const toggle = $(this);
+  var width = Math.round(toggle.outerWidth(true));
+  var top = Math.round(toggle.offset().top);
+  var left = Math.round(toggle.offset().left);
+  var right = left + width;
 
-  $('.JS_toggle-semsearch, .JS_semsearch').hover(function() {
-    $('.JS_semsearch').addClass('multiSelector--visible');
-  }, function() {
-    $('.JS_semsearch').removeClass('multiSelector--visible');
+  var popup = $('.JS_semsearch');
+  popupWidth = $(popup).width();
+  newLeft = right - popupWidth;
+
+  $(popup).css({
+    left: newLeft,
+    top: top
   });
+  $(popup).addClass('multiSelector--visible');
+}, function() {
+  $('.JS_semsearch').removeClass('multiSelector--visible');
+});
 
+$('.JS_semsearch').hover(function() {
+  $(this).addClass('multiSelector--visible');
+}, function() {
+  $(this).removeClass('multiSelector--visible');
 });
