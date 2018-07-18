71d0d17997d7f378809152b969eada0ff0831a80
Nóra Szepes <nora.szepes@tenforce.com>
2018-07-18 16:54:15 +0200

    Feild.js: added setup action

--- scripts/components/field.js
+++ scripts/components/field.js
@@ -1,48 +1,52 @@
+setupFieldActions();
 
-$('.field').each(function() {
-  var field = $(this);
-  var label = $(this).find('.field__label');
-  var input = $(this).find('.field__input');
-
-  if (input.val()) {
-    field.addClass('field--not-empty');
-  }
-
-  input.on('focus', function() {
-    field.addClass('field--focus');
-    openOverlay(true, false, false);
-  });
-
-  input.on('focusout', function() {
-    field.removeClass('field--focus');
-    closeOverlay();
+function setupFieldActions(){
+  $('.field').each(function() {
+    var field = $(this);
+    var label = $(this).find('.field__label');
+    var input = $(this).find('.field__input');
 
     if (input.val()) {
       field.addClass('field--not-empty');
     }
-    else {
-      field.removeClass('field--not-empty');
-    }
+
+    input.on('focus', function() {
+      field.addClass('field--focus');
+      openOverlay(true, false, false);
+    });
+
+    input.on('focusout', function() {
+      field.removeClass('field--focus');
+      closeOverlay();
+
+      if (input.val()) {
+        field.addClass('field--not-empty');
+      }
+      else {
+        field.removeClass('field--not-empty');
+      }
+    });
   });
-});
 
-$('.header__inputTitle').on('focus', function() {
-  $(this).addClass("header__inputTitle--focused");
-  openOverlay(true, false, false);
-  $(this).select();
+  $('.header__inputTitle').on('focus', function() {
+    $(this).addClass("header__inputTitle--focused");
+    openOverlay(true, false, false);
+    $(this).select();
 
-});
-$('.header__inputTitle').on('focusout', function() {
-  $(this).removeClass("header__inputTitle--focused");
-  closeOverlay();
-});
+  });
+  $('.header__inputTitle').on('focusout', function() {
+    $(this).removeClass("header__inputTitle--focused");
+    closeOverlay();
+  });
 
-// for inputs outside of fields show overlay-body--text on focus
-$('.field__input').on('focus', function() {
-  openOverlay(true, false, false);
-}).on('focusout', function() {
-  closeOverlay();
-});
+  // for inputs outside of fields show overlay-body--text on focus
+  $('.field__input').on('focus', function() {
+    openOverlay(true, false, false);
+  }).on('focusout', function() {
+    closeOverlay();
+  });
+}
+
 
 
 /*
